fc78e5e99b08d8614d290bd6473c528abb984aa6
Fabian <fabian13pl@gmail.com>
2020-09-26 15:34:42 +0200

    Task 3 complite

--- src/fabian/task/Animal.java
+++ src/fabian/task/Animal.java
@@ -4,6 +4,22 @@ public class Animal {
     private MapDirection mapDirection = MapDirection.NORTH;;
     private Vector2d vector = new Vector2d(2,2);
 
+    public MapDirection getMapDirection() {
+        return mapDirection;
+    }
+
+    public Vector2d getVector() {
+        return vector;
+    }
+
+    public void setMapDirection(MapDirection mapDirection) {
+        this.mapDirection = mapDirection;
+    }
+
+    public void setVector(Vector2d vector) {
+        this.vector = vector;
+    }
+
     @Override
     public String toString() {
         return vector.toString()+" "+mapDirection.toString();
---
+++ src/fabian/task/World.java
@@ -0,0 +1,8 @@
+package fabian.task;
+
+public class World {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        System.out.println(animal1.toString());
+    }
+}
